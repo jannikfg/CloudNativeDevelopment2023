a415ed270ceb5e0585da3fb0a8a8b67a8052571e
Jannik Füllgraf <jannikfuel@googlemail.com>
2024-01-03 09:56:05 +0100

    Add Dependency Test

--- UserService/src/test/java/thi/cnd/DependencyTest.java
+++ UserService/src/test/java/thi/cnd/DependencyTest.java
@@ -1,5 +1,17 @@
 package thi.cnd;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+@AnalyzeClasses(packages = "thi.cnd")
 public class DependencyTest {
+
+  @ArchTest
+  static final ArchRule REST_SHOULD_NOT_ACCESS_JPA = noClasses().that()
+      .resideInAnyPackage("..adapter.api..").should().accessClassesThat()
+      .resideInAPackage("..adapter.jpa..");
 
 }
